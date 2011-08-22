8f799a442a05cc7713c6b2fbee48a57d156f830d
Austen Ito <austen.services@gmail.com>
2011-08-21 23:09:38 -1000

    Initial commit of the dealios chrome extension.

---
+++ fancy-options/i18n.js
@@ -0,0 +1,31 @@
+// SAMPLE
+this.i18n = {
+    "settings": {
+        "en": "Settings",
+        "de": "Optionen"
+    },
+    "search": {
+        "en": "Search",
+        "de": "Suche"
+    },
+    "nothing-found": {
+        "en": "No matches were found.",
+        "de": "Keine Übereinstimmungen gefunden."
+    },
+
+    "feed": {
+        "en": "Feed",
+    },
+    
+    "location": {
+        "en": "Location",
+    },
+
+    "location-label": {
+        "en": "What city are you in?",
+    },
+
+    "location-description": {
+        "en": "Selecting your location will allow us to aggregate the deals.",
+    },
+};
---
+++ fancy-options/manifest.js
@@ -0,0 +1,32 @@
+this.manifest = {
+    "name": "Dealios",
+    "icon": "../images/dealios48.png",
+    "settings": [
+        {
+            "tab": i18n.get("feed"),
+            "group": i18n.get("location"),
+            "name": "dealios-location-desc",
+            "type": "description",
+            "text": i18n.get("location-description"),
+        },
+        {
+            "tab": i18n.get("feed"),
+            "group": i18n.get("location"),
+            "name": "dealios-location",
+            "type": "popupButton",
+            "label": i18n.get("location-label"),
+            "options": {
+                "values": [
+                    {
+                        "value": "chicago",
+                        "text": "Chicago, IL",
+                    },
+                    {
+                        "value": "honolulu",
+                        "text": "Honolulu, HI",
+                    },
+                ],
+            },
+        },
+    ],
+};
